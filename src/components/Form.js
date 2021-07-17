5b8debf411bf09bf36eba9c94e3dc33dcc8df2ae
dasurahul <dasurahul598@gmail.com>
2021-07-17 16:19:17 +0530

    My Commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -128,7 +128,6 @@ const Form = (props) => {
             aria-hidden="true"
             style={{
               paddingRight: "20px",
-              color: input.trim().length > 0 ? "var(--primary-color)" : "#aaa",
             }}
           ></i>
         </FormControl>
