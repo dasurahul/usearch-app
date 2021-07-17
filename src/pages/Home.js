b8d6d22721a1741b48fee34ca97e1886077df0fb
dasurahul <dasurahul598@gmail.com>
2021-07-17 20:10:52 +0530

    My Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Form from "../components/Form";
 
@@ -8,6 +9,7 @@ const Home = () => {
   }, []);
   return (
     <div>
+      <Navbar />
       <div style={{ marginTop: "80px", marginBottom: "40px" }}>
         <Header />
       </div>
