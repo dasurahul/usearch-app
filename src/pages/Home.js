b8fe0cb1468d4084507392758f9e5053dc27f573
dasurahul <dasurahul598@gmail.com>
2021-07-17 19:46:09 +0530

    My Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,46 +12,6 @@ const Home = () => {
         <Header />
       </div>
       <Form />
-      <h1
-        style={{
-          textAlign: "center",
-          margin: "60px 0",
-          marginBottom: "30px",
-          color: "#333",
-        }}
-      >
-        Your Entry Point to the World of Web Data
-      </h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "30px",
-          fontSize: "20px",
-        }}
-      >
-        <div style={{ display: "flex", gap: "8px" }}>
-          <i
-            style={{ color: "var(--secondary-color)" }}
-            className="fas fa-check-circle"
-          ></i>{" "}
-          <div>API Access</div>
-        </div>
-        <div style={{ display: "flex", gap: "8px" }}>
-          <i
-            style={{ color: "var(--secondary-color)" }}
-            className="fas fa-check-circle"
-          ></i>{" "}
-          <div>AI-Powered Search</div>
-        </div>
-        <div style={{ display: "flex", gap: "8px" }}>
-          <i
-            style={{ color: "var(--secondary-color)" }}
-            className="fas fa-check-circle"
-          ></i>{" "}
-          <div>Private Search</div>
-        </div>
-      </div>
     </div>
   );
 };
